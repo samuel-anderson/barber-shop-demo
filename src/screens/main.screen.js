a5bde5f2a83da8d1509172f3a82e3e05b1fe720b
samuel-anderson <s07241990@gmail.com>
2023-12-25 15:48:24 -0800

    update buttons

--- src/screens/main.screen.js
+++ src/screens/main.screen.js
@@ -1,15 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Button } from "react-native-paper";
+import { useTheme } from "styled-components";
+import { Spacer } from "../components/spacer/spacer.component";
 
 export const MainScreen = ({ navigation }) => {
+  const theme = useTheme();
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.navigate("Barber")}>
-        Schedule Appointment
-      </Button>
-      <Button onPress={() => navigation.navigate("Access")}>
-        Barber Access
-      </Button>
+      <TouchableOpacity onPress={() => navigation.navigate("Barber")}>
+        <Button
+          icon="calendar-clock-outline"
+          style={{ backgroundColor: theme.colors.bg.tertiary }}
+        >
+          <Text style={{ color: theme.colors.text.inverse }}>
+            Schedule Appointment
+          </Text>
+        </Button>
+      </TouchableOpacity>
+      <Spacer />
+      <Spacer />
+      <TouchableOpacity onPress={() => navigation.navigate("Barber")}>
+        <Button
+          icon="account"
+          style={{ backgroundColor: theme.colors.bg.tertiary }}
+        >
+          <Text style={{ color: theme.colors.text.inverse }}>
+            Barber Access
+          </Text>
+        </Button>
+      </TouchableOpacity>
     </View>
   );
 };
