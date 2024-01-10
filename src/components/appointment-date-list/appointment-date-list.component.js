85eac16d4f102de05c6abe20f6bb4234b830bfc9
samuel-anderson <s07241990@gmail.com>
2024-01-09 16:41:00 -0800

    adding a filter

--- src/components/appointment-date-list/appointment-date-list.component.js
+++ src/components/appointment-date-list/appointment-date-list.component.js
@@ -5,35 +5,57 @@ import { SafeArea } from "../utility/safe-area.component";
 import moment from "moment";
 import { useNavigation } from "@react-navigation/native";
 import { sortArrayOfDateStrings } from "../../util/date";
+import RNPickerSelect from "react-native-picker-select";
+import { useState } from "react";
 
 export const AppointmentDateList = ({ appointments }) => {
   const navigation = useNavigation();
   const sortedDates = Object.keys(appointments).sort(sortArrayOfDateStrings);
 
-  return (
-    <View style={{ width: "100%", padding: 10 }}>
-      {sortedDates.map((date) => {
-        const formattedDate = moment(date, "YYYY_MM_DD").format(
-          "MMM. Do, YYYY"
-        );
+  const [selectedValue, setSelectedValue] = useState(null);
 
-        return (
-          <Spacer position="top" size="small" key={formattedDate}>
-            <CustomButton
-              text={formattedDate}
-              variant="dark"
-              buttonOptions={{
-                style: { borderRadius: 10 },
-                onPress: () => {
-                  navigation.navigate("Select Appointment", {
-                    appointments: appointments[date],
-                  });
-                },
-              }}
-            />
-          </Spacer>
-        );
-      })}
-    </View>
+  const placeholder = {
+    label: "Select a filter...",
+    value: null,
+  };
+
+  const items = [
+    { label: "Option 1", value: "option1" },
+    { label: "Option 2", value: "option2" },
+    { label: "Option 3", value: "option3" },
+  ];
+
+  return (
+    <>
+      <RNPickerSelect
+        onValueChange={(value) => setSelectedValue(value)}
+        items={items}
+        placeholder={placeholder}
+      />
+      <View style={{ width: "100%", padding: 20 }}>
+        {sortedDates.map((date) => {
+          const formattedDate = moment(date, "YYYY_MM_DD").format(
+            "MMM. Do, YYYY"
+          );
+
+          return (
+            <Spacer position="top" size="small" key={formattedDate}>
+              <CustomButton
+                text={formattedDate}
+                variant="dark"
+                buttonOptions={{
+                  style: { borderRadius: 10 },
+                  onPress: () => {
+                    navigation.navigate("Select Appointment", {
+                      appointments: appointments[date],
+                    });
+                  },
+                }}
+              />
+            </Spacer>
+          );
+        })}
+      </View>
+    </>
   );
 };
