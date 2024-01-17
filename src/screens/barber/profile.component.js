ea2759d009b91117b2a3d946587eca78b84e28db
samuel-anderson <s07241990@gmail.com>
2024-01-17 12:51:02 -0800

    building profile screen

--- src/screens/barber/profile.component.js
+++ src/screens/barber/profile.component.js
@@ -1,17 +1,45 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { View } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { Text } from "../../components/typography/text.component";
+import { selectBarberWithCurrentUser } from "../../redux/professionals/professionalsSelector";
 
 export const Profile = () => {
   const dispatch = useDispatch();
+  const currentUser = useSelector(selectBarberWithCurrentUser);
+  const { id, name, email, phoneNumber, schedule, services, socialMedia } =
+    currentUser;
+
+  const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   return (
-    <SafeArea
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <View style={{ width: "80%" }}>
-        <Text>Profile Screen</Text>
+    <SafeArea>
+      <View>
+        <Text>{name}</Text>
+        <Text>{email}</Text>
+        <Text>{phoneNumber}</Text>
+        {daysOfWeek.map((day) => (
+          <View key={day} style={{ margin: 20 }}>
+            <Text>{day}</Text>
+            {schedule[day.toLowerCase()] ? (
+              schedule[day.toLowerCase()].map((timeSlot, index) => (
+                <Text key={index}>
+                  {timeSlot.start} - {timeSlot.end}
+                </Text>
+              ))
+            ) : (
+              <Text>Not working</Text>
+            )}
+          </View>
+        ))}
       </View>
     </SafeArea>
   );
