56cc7b9a2d19708ee667dad8cb0fa86df0ba181a
samuel-anderson <s07241990@gmail.com>
2024-01-24 13:23:53 -0800

    touchable

--- src/screens/barber/reports.component.js
+++ src/screens/barber/reports.component.js
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { Text } from "../../components/typography/text.component";
+import { CustomButton } from "../../components/custom-button/custom-button.component";
 
 export const Reports = () => {
   return (
@@ -8,7 +9,9 @@ export const Reports = () => {
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
       <View style={{ width: "80%" }}>
-        <Text>Reports Screen</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <CustomButton variant="dark" text="Generate Report" />
+        </TouchableOpacity>
       </View>
     </SafeArea>
   );
