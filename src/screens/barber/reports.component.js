c10d2bdc196007f2b248ccd39d1ef9884e1674ec
samuel-anderson <s07241990@gmail.com>
2024-01-26 13:42:36 -0800

    add selector

--- src/screens/barber/reports.component.js
+++ src/screens/barber/reports.component.js
@@ -1,15 +1,47 @@
-import { TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 import { SafeArea } from "../../components/utility/safe-area.component";
 import { Text } from "../../components/typography/text.component";
 import { CustomButton } from "../../components/custom-button/custom-button.component";
+import {
+  SelectList,
+  MultipleSelectList,
+} from "react-native-dropdown-select-list";
 
 export const Reports = () => {
+  const [selectedStatuses, setSelectedStatuses] = useState([]);
+
+  const statuses = [
+    { key: "1", value: "Pending" },
+    { key: "2", value: "Cancelled" },
+    { key: "3", value: "Paid" },
+  ];
+
   return (
-    <SafeArea
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <View style={{ width: "80%" }}>
-        <TouchableOpacity onPress={() => {}}>
+    <SafeArea>
+      <View
+        style={{
+          padding: 20,
+          flex: 1,
+          justifyContent: "space-between",
+        }}
+      >
+        <ScrollView>
+          <Text>Start Date - End Date</Text>
+
+          <Text>Status</Text>
+
+          <MultipleSelectList
+            setSelected={(val) => setSelectedStatuses(val)}
+            onSelect={() => console.log(selectedStatuses)}
+            data={statuses}
+            save="value"
+            label="Statuses"
+            placeholder="Select Status"
+          />
+        </ScrollView>
+
+        <TouchableOpacity onPress={() => {}} style={{ zIndex: -100 }}>
           <CustomButton variant="dark" text="Generate Report" />
         </TouchableOpacity>
       </View>
