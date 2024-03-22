ec52c292758d5ecdb034eb6534362d45c7612330
samuel-anderson <s07241990@gmail.com>
2024-03-21 18:56:30 -0700

    npm commands

---
+++ app.config.js
@@ -0,0 +1,17 @@
+export default {
+  expo: {
+    ios: {
+      bundleIdentifier: "com.legacy.barbershop",
+    },
+    icon: "./assets/icon-1024.png",
+    extra: {
+      eas: {
+        projectId: "c207def8-09b6-4cc0-a2ad-0edde7378ab8",
+      },
+    },
+    updates: {
+      url: "https://u.expo.dev/c207def8-09b6-4cc0-a2ad-0edde7378ab8",
+    },
+    runtimeVersion: "1.0.9",
+  },
+};
